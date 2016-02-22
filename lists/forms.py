7b5c6080112e506b7a81f95c187760dc3634b8da
Reinis Danne <rei4dan@gmail.com>
2016-02-22 15:22:51 +0200

    lists: Use duplicate item validation in view

--- lists/forms.py
+++ lists/forms.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 from lists.models import Item
 
 
-DUPLICATE_ITEM_ERROR = "You've alredy got this in your list"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 
@@ -44,4 +44,8 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
 
